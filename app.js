f33deccceb20d7c2f821daac559a63e0adac0739
mahmudunnaby <mahmudunnabyemon0@gmail.com>
2022-03-02 03:16:13 +0600

    search by id linked with button

--- app.js
+++ app.js
@@ -22,26 +22,57 @@ const getPhoneResults = phones => {
     numResults.innerText = phones.length
     // console.log(phones.length)
 
-    for (const phone of phones) {
-        console.log(phone)
-        const informationOfPhone = `
-       
-            <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Name:${phone.phone_name}</h5>
-                <h5 class="card-title">Brand: ${phone.brand}</h5>
-                <button class="btn btn-primary ">Details</button>
-            </div>
-        
-        `
+
+
+    // for (const phone of phones) {
+
+    //     const div = document.createElement('div')
+    //     div.classList = 'card text-center col-sm-12 col-md-3  mx-3 my-3'
+
+    //     const informationOfPhone = `
+
+    //     <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
+    //     <div class="card-body">
+    //         <h5 class="card-title">Name:${phone.phone_name}</h5>
+    //         <h5 class="card-title">Brand: ${phone.brand}</h5>
+    //         <button onclick="getDetailsPhone(${phone.slug})" class="btn btn-primary ">Details</button>
+    //     </div>`
+
+
+    //     div.innerHTML = informationOfPhone
+    //     document.getElementById('resultsContainer').appendChild(div)
+
+    // }
+
+
+    phones.forEach(phone => {
+
         const div = document.createElement('div')
         div.classList = 'card text-center col-sm-12 col-md-3  mx-3 my-3'
+
+        const informationOfPhone = `
+
+        <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Name:${phone.phone_name}</h5>
+            <h5 class="card-title">Brand: ${phone.brand}</h5>
+            <button onclick="getDetailsPhone('${phone.slug}')" class="btn btn-primary ">Details</button>
+        </div>`
+
+
         div.innerHTML = informationOfPhone
         document.getElementById('resultsContainer').appendChild(div)
 
-
-        // console.log(informationOfPhone)
-    }
-    // phones.forEach(phone => console.log(phone))
+    })
 
 }
+
+
+const getDetailsPhone = id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    console.log(url)
+    // fetch(url)
+    //     .then(responce => responce.json())
+    //     .then(data => console.log(data))
+}
+
